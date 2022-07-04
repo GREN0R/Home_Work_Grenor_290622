fc6c92485b8e1405bf8212f35986518b521d3f1f
Grenor <sirius1light@gmail.com>
2022-07-04 22:12:40 +0300

    123

--- src/test/java/part2/ReplaceAll.java
+++ src/test/java/part2/ReplaceAll.java
@@ -1,10 +1,14 @@
 package part2;
-
+//частина2
+//        Робота зі строками
+//        1. вивести строку без голосних
+//        2. Вивести строку без приголосних
+//        3. Вивести речення без пробілів
 public class ReplaceAll {
     public static void main(String[] args) {
         String test = "Hello Grenor";
-        System.out.println(test.replaceAll("[ ]",""));
         System.out.println(test.replaceAll("[eo]",""));
         System.out.println(test.replaceAll("[HlGrn]",""));
+        System.out.println(test.replaceAll("[ ]",""));
     }
 }
